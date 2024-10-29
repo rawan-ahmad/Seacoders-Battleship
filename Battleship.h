ed75dcf5b4efb9db5c665891630f0ae202314306
rawan-ahmad <rna55@mail.aub.edu>
2024-10-29 19:22:32 +0200

    creating and printing the grid + change from char to indices

---
+++ Battleship.h
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+// game functions will be here
+
+// 1. to delay time before clearing for smoother transition
+void delay(int number_of_seconds)
+{
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds)
+        ;
+}
+
+// 2. function to create the grid
+void createArray(char **grid, const int SIZE)
+{
+    // filling the array
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            grid[i][j] = '~'; // to fill the wave
+        }
+    }
+}
+
+// 3. function to print the array with the ships for locating
+void printArray(char **grid, const int SIZE)
+{
+    printf("\n   ");
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf(" %c ", 'A' + i); // to print the letter coords
+    }
+    printf("\n");
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf(" %d ", i); // to print the number coords
+        for (int j = 0; j < SIZE; j++)
+        {
+            printf(" %c ", grid[i][j]); // to print the wave or ship
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// 4. function to print the array with the hits and misses
+void printGrid(char **grid, const int SIZE)
+{
+    printf("\n   ");
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf(" %c ", 'A' + i);
+    }
+    printf("\n");
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf(" %d ", i);
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (grid[i][j] == 'o' || grid[i][j] == '*')
+                printf(" %c ", grid[i][j]);
+            else
+                printf(" ~ ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// 5. function to invert from char (ascii value of char) to indices 
+void invert(int *c, int *r)
+{
+    *c = *c - 'A';
+    *r = *r - '0';
+}
